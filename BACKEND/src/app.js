5a5cac0d50342703591a34b75e7022ebb9c601ae
harsh-85293 <harsh.ramchandani122003@gmail.com>
2025-08-29 01:01:16 +0530

    feat: make API URL configurable; allow Vercel origin in CORS and Socket.IO

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -10,9 +10,15 @@ const User = require("./models/user");
 const cookieParser = require("cookie-parser")
 const cors = require("cors")
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5174",
+  process.env.FRONTEND_URL,
+].filter(Boolean);
+
 app.use(cors({
-  origin : ["http://localhost:5173", "http://localhost:5174"],
-  credentials : true,
+  origin: allowedOrigins,
+  credentials: true,
 }))
 app.use(express.json());
 app.use(cookieParser())
@@ -34,7 +40,7 @@ app.use("/", paymentsPhonePeRouter);
 // Socket.io setup
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: allowedOrigins,
     credentials: true,
   }
 });
